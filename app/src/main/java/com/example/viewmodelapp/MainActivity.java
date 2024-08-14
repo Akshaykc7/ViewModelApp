09764c7c3488130408d25d467383170a51fd0c8e
Akshay K C <akshaykc016@gmail.com>
2024-08-14 13:42:02 +0530

    ViewModel App

--- app/src/main/java/com/example/viewmodelapp/MainActivity.java
+++ app/src/main/java/com/example/viewmodelapp/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.viewmodelapp.databinding.ActivityMainBinding;
@@ -30,14 +31,15 @@ public class MainActivity extends AppCompatActivity {
         });
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+        activityMainBinding.setMyViewModel(myViewModel);
 
-        activityMainBinding.button.setOnClickListener(new View.OnClickListener() {
+        //Observing livedata
+        myViewModel.getCounter().observe(this, new Observer<Integer>() {
             @Override
-            public void onClick(View v) {
-                myViewModel.increaseCounter();
-                activityMainBinding.textView2.setText("" + myViewModel.getCounter());
+            public void onChanged(Integer counter) {
+                activityMainBinding.textView2.setText("" + counter);
+
             }
         });
-        activityMainBinding.textView2.setText("" + myViewModel.getCounter());
     }
 }
--- app/src/main/java/com/example/viewmodelapp/MyViewModel.java
+++ app/src/main/java/com/example/viewmodelapp/MyViewModel.java
@@ -1,16 +1,21 @@
 package com.example.viewmodelapp;
 
+import android.view.View;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
+    private MutableLiveData<Integer> counter = new MutableLiveData<>();
 
-    int counter = 0;
 
-    public void increaseCounter(){
-        counter++;
+    public void increaseCounter(View view){
+        int currentValue = counter.getValue() !=null ? counter.getValue():0;
+        counter.setValue(currentValue + 1);
     }
 
-    public int getCounter() {
+    public LiveData<Integer> getCounter() {
         return counter;
     }
 }
